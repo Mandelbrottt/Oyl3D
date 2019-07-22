47f9bf589aaaaee26fa67f95206e3699be80f5c2
Myles Cragg <rocketpod@hotmail.com>
2019-07-22 18:17:53 -0400

    Added OpenGL Debug Callback Functionality

--- Engine/src/Oyl3D/Common.h
+++ Engine/src/Oyl3D/Common.h
@@ -9,10 +9,15 @@
 #define BIND_CALLBACK_1(x) std::bind(&x, this, std::placeholders::_1)
 #define BIND_CALLBACK(x) BIND_CALLBACK_1(x)
 
+
 #if defined(OYL_DEBUG)
-#	define ASSERT(x, ...) { if(!x) { LOG_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#	define VERIFY(x, ...) { if(!x) { LOG_ERROR("Verification failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#	if defined(_MSC_VER)
+#		define BREAKPOINT __debugbreak()
+#	endif
+#	define ASSERT(x, ...) { if(!x) { LOG_ERROR("Assertion failed: {0}", __VA_ARGS__); BREAKPOINT; } }
+#	define VERIFY(x, ...) { if(!x) { LOG_ERROR("Verification failed: {0}", __VA_ARGS__); BREAKPOINT; } }
 #else
+#	define BREAKPOINT()
 #	define ASSERT(x) 
 #	define VERIFY(x) x
 #endif
--- Engine/src/Oyl3D/Platform/OpenGL/OpenGLContext.cpp
+++ Engine/src/Oyl3D/Platform/OpenGL/OpenGLContext.cpp
@@ -6,6 +6,22 @@
 
 namespace oyl {
 
+static void OpenGLErrorCallback(GLenum source,
+								GLenum type,
+								GLuint id,
+								GLenum severity,
+								GLsizei length, 
+								const GLchar* message,
+								const void* userParam) {
+	// Ignore List
+	switch (id) {
+	case 131185:
+		return;
+	}
+	LOG_ERROR("OpenGL Error({0}) at {1}:{2}\n\t   {3}", id, __FILE__, __LINE__, message);
+	BREAKPOINT;
+}
+
 OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
 	: m_windowHandle(windowHandle) {
 	ASSERT(windowHandle, "Window handle is null!");
@@ -15,6 +31,11 @@ void OpenGLContext::init() {
 	glfwMakeContextCurrent(m_windowHandle);
 	int status = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 	ASSERT(status, "Failed to initialize Glad!");
+
+#if !defined(OYL_DIST)
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(OpenGLErrorCallback, 0);
+#endif
 
 	LOG_INFO("OpenGL Info:");
 	LOG_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
