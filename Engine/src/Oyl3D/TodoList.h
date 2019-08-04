9de2bc690b12cd8747b7e734fab4ac32242f3e8d
Myles Cragg <rocketpod@hotmail.com>
2019-08-03 21:19:27 -0400

    Added Todo List To Source Cotrol
    TodoList is a .h file so that the comments show up in the Task List in VS

---
+++ Engine/src/Oyl3D/TodoList.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// TODO: Multi-Thread Engine
+// TODO: Add Queueing to Render Commands (Batching Calls Based On Shader/Texture Used and Whatnot)
+// TODO: Think About Scene Graph
+// TODO: Make Better Event Dispatcher
+// TODO: Add Audio Functionality
+// TODO: Flesh Out Model Loader (Swap to Collada Files for Dynamic Meshes, Keep to Wavefront for Static Maybe?)
+// TODO: Start Working on PBR (Far Down the Line, Need to Establish Art Style With Team To Determine If It's Worth the Time)
+// TODO: Think About How Physics Engine Should Work (Physics World?)
+// TODO: Think About How Scenes Should Work (App Owns Scenes and Scenes Own Layers?)
