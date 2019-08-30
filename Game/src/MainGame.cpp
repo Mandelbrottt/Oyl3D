3d459767bedb601beca4ee96677b694ceb0bf726
Myles Cragg <cragg.myles@gmail.com>
2019-08-30 16:39:12 -0400

    Added Generic MainGame Template

--- Game/src/MainGame.cpp
+++ Game/src/MainGame.cpp
@@ -1,14 +1,43 @@
 #include <Oyl3D.h>
 
+class MainLayer : public oyl::Layer {
+public:
+	MainLayer() : Layer("MainLayer") {}
+
+	virtual void onAttach() override {}
+
+	virtual void onDetach() override {}
+
+	virtual void onUpdate(oyl::Timestep dt) override {}
+
+	virtual void onImGuiRender() override {}
+
+	virtual void onEvent(oyl::Event& event) override {}
+public:
+	OYL_CREATE_FUNC(MainLayer)
+};
+
+class MainScene : public oyl::Scene {
+public:
+	MainScene() : oyl::Scene("MainScene") {}
+	~MainScene() {}
+
+	virtual void onEnter() {
+		pushLayer(MainLayer::create());
+	}
+
+	virtual void onExit() {}
+public:
+	OYL_CREATE_FUNC(MainScene)
+};
+
 class Game : public oyl::Application {
 public:
 	Game() {
-
+		pushScene(MainScene::create());
 	}
 
-	~Game() {
-
-	}
+	~Game() {}
 };
 
 oyl::Application* oyl::createApplication() {
