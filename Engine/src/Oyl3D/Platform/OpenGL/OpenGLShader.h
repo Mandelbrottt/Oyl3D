cf5b27a39079a63a69937bfe7827790212ad44be
Myles Cragg <cragg.myles@gmail.com>
2019-09-01 19:47:18 -0400

    Adjusted OpenGLShader With Comments and Less Copy Paste

--- Engine/src/Oyl3D/Platform/OpenGL/OpenGLShader.h
+++ Engine/src/Oyl3D/Platform/OpenGL/OpenGLShader.h
@@ -8,26 +8,24 @@ class OpenGLShader : public Shader {
 public:
 	virtual ~OpenGLShader();
 
-	virtual void bind() const;
-	virtual void unbind() const;
+	virtual void bind() const override;
+	virtual void unbind() const override;
 
 	virtual void setUniform1i(const std::string& name, const int v) override;
 	virtual void setUniform2f(const std::string& name, const glm::vec2& v) override;
 	virtual void setUniform3f(const std::string& name, const glm::vec3& v) override;
 	virtual void setUniform4f(const std::string& name, const glm::vec4& v) override;
-	virtual void setUniformMat3(const std::string& name, const glm::mat3& v) override;
-	virtual void setUniformMat4(const std::string& name, const glm::mat4& v) override;
+	virtual void setUniformMat3(const std::string& name, const glm::mat3& m) override;
+	virtual void setUniformMat4(const std::string& name, const glm::mat4& m) override;
 private:
 	friend class Shader;
 	OpenGLShader(const std::initializer_list<ShaderInfo>& files);
 
-	void processShaders(const std::string& vertSrc,
-						const std::string& tescSrc,
-						const std::string& teseSrc,
-						const std::string& geomSrc,
-						const std::string& fragSrc);
-
-	int getUniformLocation(const std::string& name) const;
+    void processShaders(std::array<std::string, NumShaderTypes> shaderSrcs);
+    uint compileShader(uint type, const std::string& src);
+    uint linkShaders(std::array<uint, NumShaderTypes> shaders);
+    
+    int getUniformLocation(const std::string& name) const;
 private:
 	uint m_rendererID = 0;
 
