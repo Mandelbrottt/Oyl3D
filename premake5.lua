fe36b812e214a405dbdf72baa3aa78c55ac40ca1
Mandelbrottt <cragg.myles@gmail.com>
2019-09-13 08:51:42 -0400

    Added Fmod as a Vendor Project

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,7 @@ IncludeDir["glad"] = "Engine/vendor/glad/include/"
 IncludeDir["imgui"] = "Engine/vendor/imgui/"
 IncludeDir["glm"] = "Engine/vendor/glm/"
 IncludeDir["stb"] = "Engine/vendor/stb/"
+IncludeDir["fmod"] = "Engine/vendor/fmod/include/"
 
 group "Dependencies"
 	include "Engine/vendor/glad/"
@@ -60,7 +61,12 @@ project "OylEngine"
 		"%{IncludeDir.glad}",
 		"%{IncludeDir.imgui}",
 		"%{IncludeDir.glm}",
-		"%{IncludeDir.stb}"
+		"%{IncludeDir.stb}",
+		"%{IncludeDir.fmod}"
+	}
+
+	libdirs {
+		"Engine/vendor/fmod/lib/"
 	}
 
 	links {
@@ -83,6 +89,10 @@ project "OylEngine"
 		defines { "OYL_DEBUG" }
 		runtime "Debug"
 		symbols "on"
+		links {
+			"fmodL_vc.lib",
+			"fmodstudioL_vc.lib"
+		}
 
 	filter "configurations:Development"
 		defines { "OYL_DEVELOPMENT" }
@@ -93,6 +103,14 @@ project "OylEngine"
 		defines { "OYL_DISTRIBUTION" }
 		runtime "Release"
 		optimize "on"
+
+	filter "configurations:not Debug"
+		links {
+			"fmod_vc.lib",
+			"fmod_studio_vc.lib"
+		}
+
+	filter "configurations:not Development"
 
 	filter "configurations:not Distribution"
 		defines { "OYL_LOG_CONSOLE" }
