6612e031830c2b442ecf372db055eb41ec26fb1c
Mandelbrottt <cragg.myles@gmail.com>
2019-09-27 08:57:01 -0400

    Organized Common.h, Added More Callback Macros

--- Engine/src/Oyl3D/Common.h
+++ Engine/src/Oyl3D/Common.h
@@ -1,72 +1,100 @@
 #pragma once
 
-#if defined (OYL_PLATFORM_WINDOWS)
-    #if defined(OYL_SHARED_LIB)
-        #if defined(OYL_ENGINE) && !defined(OYL_BUILD_DLL)
-            #define OYL_BUILD_DLL
-        #elif !defined(OYL_DLL)
-            #define OYL_DLL
+#pragma region Shared Library Building
+    #if defined (OYL_PLATFORM_WINDOWS)
+        #if defined(OYL_SHARED_LIB)
+            #if defined(OYL_ENGINE) && !defined(OYL_BUILD_DLL)
+                #define OYL_BUILD_DLL
+            #elif !defined(OYL_DLL)
+                #define OYL_DLL
+            #endif
         #endif
-    #endif
-
-    #if defined(OYL_BUILD_DLL) && defined(OYL_DLL)
-        #error You must not define both OYL_BUILD_DLL and OYL_DLL at the same time!
-    #endif
-
-    #if defined(OYL_BUILD_DLL)
-        #define OYLAPI __declspec(dllexport)
-    #elif defined(OYL_DLL)
-        #define OYLAPI __declspec(dllimport)
+    
+        #if defined(OYL_BUILD_DLL) && defined(OYL_DLL)
+            #error You must not define both OYL_BUILD_DLL and OYL_DLL at the same time!
+        #endif
+    
+        #if defined(OYL_BUILD_DLL)
+            #define OYLAPI __declspec(dllexport)
+        #elif defined(OYL_DLL)
+            #define OYLAPI __declspec(dllimport)
+        #else
+            #define OYLAPI
+        #endif
     #else
-        #define OYLAPI
+        #error Oyl3D only supports Windows!
     #endif
-#else
-    #error Oyl3D only supports Windows!
-#endif
+#pragma endregion
 
-#define OYL_CALLBACK(func)   ::std::bind(&func, this)
-#define OYL_CALLBACK_1(func) ::std::bind(&func, this, std::placeholders::_1)
-
-#define OYL_CREATE_FUNC(type) static ::oyl::Ref<type> create() { return ::oyl::Ref<type>(new type()); }
-
-#pragma warning(disable: 4002 4003)
-
-#if defined(OYL_DEBUG)
-    #define OYL_ENABLE_ASSERTS
-    #if defined(_MSC_VER)
-        #define OYL_BREAKPOINT ::__debugbreak()
-    #else
-        #define OYL_BREAKPOINT
-    #endif
-#else
-    #define OYL_BREAKPOINT
-#endif
-
-#if defined(OYL_ENABLE_ASSERTS)
+#pragma region Macro Argument Overloading
     #define _OYL_EXPAND(x) x
     #define _OYL_CAT(a, b) a##b
     #define _OYL_SELECT(name, num) _OYL_CAT(name##_, num)
     #define _OYL_GET_COUNT(_1, _2, _3, _4, _5, count, ...) count
     #define _OYL_VA_SIZE(...) _OYL_EXPAND(_OYL_GET_COUNT(__VA_ARGS__, 5, 4, 3, 2, 1))
     #define _OYL_VA_SELECT(name, ...) _OYL_EXPAND(_OYL_SELECT(name, _OYL_VA_SIZE(__VA_ARGS__))(__VA_ARGS__))
+    #define _OYL_MACRO_OVERLOAD(name, ...) _OYL_EXPAND(_OYL_VA_SELECT(name, __VA_ARGS__))
+#pragma endregion
 
-    #define OYL_ASSERT(...) _OYL_EXPAND(_OYL_VA_SELECT(_OYL_ASSERT, __VA_ARGS__))
-    #define OYL_VERIFY(...) _OYL_EXPAND(_OYL_VA_SELECT(_OYL_VERIFY, __VA_ARGS__))
+#pragma region Delegate Callbacks
+    #define OYL_CALLBACK(...)   _OYL_MACRO_OVERLOAD(_OYL_CALLBACK_, __VA_ARGS__)
+    #define OYL_CALLBACK_1(...) _OYL_MACRO_OVERLOAD(_OYL_CALLBACK_1, __VA_ARGS__)
+    #define OYL_CALLBACK_2(...) _OYL_MACRO_OVERLOAD(_OYL_CALLBACK_2, __VA_ARGS__)
+    #define OYL_CALLBACK_3(...) _OYL_MACRO_OVERLOAD(_OYL_CALLBACK_3, __VA_ARGS__)
+    
+    #define _OYL_CALLBACK__2(func, ptr) ::std::bind(&func, ptr)
+    #define _OYL_CALLBACK__1(func)      _OYL_CALLBACK__2(func, this)
+            
+    #define _OYL_CALLBACK_1_2(func, ptr) ::std::bind(&func, ptr, std::placeholders::_1)
+    #define _OYL_CALLBACK_1_1(func)      _OYL_CALLBACK_1_2(func, this)
+            
+    #define _OYL_CALLBACK_2_2(func, ptr) ::std::bind(&func, ptr, std::placeholders::_1, \
+                                                                 std::placeholders::_2)
+    #define _OYL_CALLBACK_2_1(func)      _OYL_CALLBACK_2_2(func, this)
+            
+    #define _OYL_CALLBACK_3_2(func, ptr) ::std::bind(&func, ptr, std::placeholders::_1, \
+                                                                 std::placeholders::_2, \
+                                                                 std::placeholders::_3)
+    #define _OYL_CALLBACK_3_1(func)      _OYL_CALLBACK_3_2(func, this)
+#pragma endregion
 
-    #define _OYL_ASSERT_1(expr)           { if(!(expr)) { OYL_BREAKPOINT; } }
-    #define _OYL_ASSERT_2(expr, str)      { if(!(expr)) { OYL_LOG_ERROR("Assertion failed: " str); OYL_BREAKPOINT; } }
-    #define _OYL_ASSERT_3(expr, str, ...) { if(!(expr)) { OYL_LOG_ERROR("Assertion failed: " str, __VA_ARGS__); OYL_BREAKPOINT; } }
-    #define _OYL_ASSERT_4(expr, str, ...) _OYL_ASSERT_3
-    #define _OYL_ASSERT_5(expr, str, ...) _OYL_ASSERT_3
+#pragma region Necessary Per-Class Override Functions
+    #define OYL_CREATE_FUNC(type) static ::oyl::Ref<type> create() { return ::oyl::Ref<type>(new type()); }
+#pragma endregion
 
-    #define _OYL_VERIFY_1(expr)           { if(!(expr)) { OYL_BREAKPOINT; } }
-    #define _OYL_VERIFY_2(expr, str)      { if(!(expr)) { OYL_LOG_ERROR("Verification failed: " str); OYL_BREAKPOINT; } }
-    #define _OYL_VERIFY_3(expr, str, ...) { if(!(expr)) { OYL_LOG_ERROR("Verification failed: " str, __VA_ARGS__); OYL_BREAKPOINT; } }
-    #define _OYL_VERIFY_4(expr, str, ...) _OYL_VERIFY_3
-    #define _OYL_VERIFY_5(expr, str, ...) _OYL_VERIFY_3
-#else
-    #define OYL_ASSERT(expr) 
-    #define OYL_VERIFY(expr) expr
-#endif
-
-#pragma warning(default: 4005 4003)
+#pragma region Debug Macros
+    #pragma warning(disable: 4002 4003)
+    
+    #if defined(OYL_DEBUG)
+        #define OYL_ENABLE_ASSERTS
+        #if defined(_MSC_VER)
+            #define OYL_BREAKPOINT ::__debugbreak()
+        #else
+            #define OYL_BREAKPOINT
+        #endif
+    #else
+        #define OYL_BREAKPOINT
+    #endif
+    
+    #if defined(OYL_ENABLE_ASSERTS)
+        #define OYL_ASSERT(...) _OYL_MACRO_OVERLOAD(_OYL_ASSERT, __VA_ARGS__)
+        #define OYL_VERIFY(...) _OYL_MACRO_OVERLOAD(_OYL_ASSERT, __VA_ARGS__)
+    
+        #define _OYL_ASSERT_1(expr)           { if(!(expr)) { OYL_BREAKPOINT; } }
+        #define _OYL_ASSERT_2(expr, str)      { if(!(expr)) { OYL_LOG_ERROR("Assertion failed: " str); OYL_BREAKPOINT; } }
+        #define _OYL_ASSERT_3(expr, str, ...) { if(!(expr)) { OYL_LOG_ERROR("Assertion failed: " str, __VA_ARGS__); OYL_BREAKPOINT; } }
+        #define _OYL_ASSERT_4(expr, str, ...) _OYL_ASSERT_3
+        #define _OYL_ASSERT_5(expr, str, ...) _OYL_ASSERT_3
+    
+        #define _OYL_VERIFY_1(expr)           { if(!(expr)) { OYL_BREAKPOINT; } }
+        #define _OYL_VERIFY_2(expr, str)      { if(!(expr)) { OYL_LOG_ERROR("Verification failed: " str); OYL_BREAKPOINT; } }
+        #define _OYL_VERIFY_3(expr, str, ...) { if(!(expr)) { OYL_LOG_ERROR("Verification failed: " str, __VA_ARGS__); OYL_BREAKPOINT; } }
+        #define _OYL_VERIFY_4(expr, str, ...) _OYL_VERIFY_3
+        #define _OYL_VERIFY_5(expr, str, ...) _OYL_VERIFY_3
+    #else
+        #define OYL_ASSERT(expr) 
+        #define OYL_VERIFY(expr) expr
+    #endif
+    
+    #pragma warning(default: 4005 4003)
+#pragma endregion
