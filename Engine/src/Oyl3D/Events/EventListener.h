e20cdfabdd81c07a764d03724c96cc7b53432dd6
Mandelbrottt <cragg.myles@gmail.com>
2019-10-01 19:16:08 -0400

    Added Event Category Mask

--- Engine/src/Oyl3D/Events/EventListener.h
+++ Engine/src/Oyl3D/Events/EventListener.h
@@ -6,24 +6,32 @@ namespace oyl
 {
     class IEventListener
     {
+    protected:
+        static const u32 MAX_EVENT_TYPES = 128;
+        static const u32 MAX_CATEGORIES  = 32;
     public:
         using Priority = u32;
-        using EventMask = std::bitset<128>;
-        
+        using EventMask = std::bitset<MAX_EVENT_TYPES>;
+        using CategoryMask = std::bitset<MAX_CATEGORIES>;
+
         using PostEventFn = std::function<void(UniqueRef<Event>)>;
         using RegisterFn = std::function<void(Ref<IEventListener>, Priority)>;
         using UnregisterFn = std::function<void(Ref<IEventListener>)>;
 
     public:
-        explicit IEventListener() = default;
-        virtual ~IEventListener() = default;
+        explicit IEventListener()  = default;
+        virtual  ~IEventListener() = default;
 
         virtual bool onEvent(Ref<Event> event) = 0;
 
         virtual void postEvent(UniqueRef<Event> event) = 0;
 
-        virtual const EventMask& getEventMask() const = 0;
-        
+        virtual const EventMask&    getEventMask() const = 0;
+        virtual const CategoryMask& getCategoryMask() const = 0;
+
+        virtual void addToEventMask(u32 eventType) = 0;
+        virtual void addToCategoryMask(u32 eventCategory) = 0;
+
         virtual void setPostEventCallback(PostEventFn callback) = 0;
         virtual void setRegisterCallback(RegisterFn callback) = 0;
         virtual void setUnregisterCallback(UnregisterFn callback) = 0;
@@ -34,14 +42,18 @@ namespace oyl
         friend class EventDispatcher;
     public:
         explicit EventListener();
-        virtual ~EventListener();
-        
+        virtual  ~EventListener();
+
         virtual bool onEvent(Ref<Event> event) override;
-        
+
         virtual void postEvent(UniqueRef<Event> event) override;
 
-        virtual const EventMask& getEventMask() const override { return m_eventMask; }
-        
+        virtual const EventMask&    getEventMask() const override { return m_eventMask; }
+        virtual const CategoryMask& getCategoryMask() const override { return m_categoryMask; }
+
+        virtual void addToEventMask(u32 eventType) override { m_eventMask[eventType] = true; }
+        virtual void addToCategoryMask(u32 eventCategory) override { m_categoryMask[eventCategory] = true; }
+
         virtual void setPostEventCallback(PostEventFn callback) override
         {
             m_postEventCallback = std::move(callback);
@@ -56,12 +68,13 @@ namespace oyl
         {
             m_unregisterCallback = std::move(callback);
         }
-        
-    protected:
-        EventMask m_eventMask;
 
-        PostEventFn m_postEventCallback;
-        RegisterFn m_registerCallback;
+    protected:
+        EventMask    m_eventMask;
+        CategoryMask m_categoryMask;
+
+        PostEventFn  m_postEventCallback;
+        RegisterFn   m_registerCallback;
         UnregisterFn m_unregisterCallback;
     };
 }
