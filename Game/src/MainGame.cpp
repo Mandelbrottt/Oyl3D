ecb0f29cb71e0e838b8346701eb954e91f29c472
Mandelbrottt <cragg.myles@gmail.com>
2019-11-27 20:47:15 -0500

    removed animation from maingame

--- Game/src/MainGame.cpp
+++ Game/src/MainGame.cpp
@@ -39,58 +39,58 @@ public:
         }
 
         // TEMPORARY:
-        {
-            auto e = registry->create();
-         
-            auto& t = registry->assign<component::Transform>(e);
-            t.setPosition(glm::vec3(3.0f, 3.0f, 3.0f));
-
-            auto& so = registry->assign<component::SceneObject>(e);
-            so.name = "Animation Object";
-
-            auto& mr = registry->assign<component::Renderable>(e);
-            mr.mesh = Mesh::cache("res/assets/models/agony/agony1_000001.obj");
-            mr.material = animMat;
-
-            char filename[512];
-
-            std::string s;
-            s.reserve(512);
-         
-            auto anim1 = Ref<component::Animation>::create();
-            auto anim2 = Ref<component::Animation>::create();
-
-            int scale = 10;
-            
-            for (int i = 0; i < 205 / scale; i++)
-            {
-                component::Animation::KeyPose kp;
-                kp.duration = (1.0f / 30.0f) * scale;
-
-                sprintf(filename, "res/assets/models/default/dancemoves_%06d.obj", i * scale + 1);
-                s.assign(filename);
-             
-                kp.mesh = Mesh::cache(s);
-                anim1->poses.push_back(kp);
-            }
-
-            for (int i = 0; i < 65 / scale; i++)
-            {
-                component::Animation::KeyPose kp;
-                kp.duration = (1.0f / 30.0f) * (float) scale;
-
-                sprintf(filename, "res/assets/models/boxing/boxing_%06d.obj", i * scale + 1);
-                s.assign(filename);
-
-                kp.mesh = Mesh::create(s);
-                anim2->poses.push_back(kp);
-            }
-
-            auto& anim = registry->assign<component::Animator>(e);
-
-            anim.pushAnimation("agony", anim1);
-            anim.pushAnimation("boxing", anim2);
-        }
+        //{
+        //    auto e = registry->create();
+        // 
+        //    auto& t = registry->assign<component::Transform>(e);
+        //    t.setPosition(glm::vec3(3.0f, 3.0f, 3.0f));
+        //
+        //    auto& so = registry->assign<component::SceneObject>(e);
+        //    so.name = "Animation Object";
+        //
+        //    auto& mr = registry->assign<component::Renderable>(e);
+        //    mr.mesh = Mesh::cache("res/assets/models/agony/agony1_000001.obj");
+        //    mr.material = animMat;
+        //
+        //    char filename[512];
+        //
+        //    std::string s;
+        //    s.reserve(512);
+        // 
+        //    auto anim1 = Ref<component::Animation>::create();
+        //    auto anim2 = Ref<component::Animation>::create();
+        //
+        //    int scale = 10;
+        //    
+        //    for (int i = 0; i < 205 / scale; i++)
+        //    {
+        //        component::Animation::KeyPose kp;
+        //        kp.duration = (1.0f / 30.0f) * scale;
+        //
+        //        sprintf(filename, "res/assets/models/default/dancemoves_%06d.obj", i * scale + 1);
+        //        s.assign(filename);
+        //     
+        //        kp.mesh = Mesh::cache(s);
+        //        anim1->poses.push_back(kp);
+        //    }
+        //
+        //    for (int i = 0; i < 65 / scale; i++)
+        //    {
+        //        component::Animation::KeyPose kp;
+        //        kp.duration = (1.0f / 30.0f) * (float) scale;
+        //
+        //        sprintf(filename, "res/assets/models/boxing/boxing_%06d.obj", i * scale + 1);
+        //        s.assign(filename);
+        //
+        //        kp.mesh = Mesh::create(s);
+        //        anim2->poses.push_back(kp);
+        //    }
+        //
+        //    auto& anim = registry->assign<component::Animator>(e);
+        //
+        //    anim.pushAnimation("agony", anim1);
+        //    anim.pushAnimation("boxing", anim2);
+        //}
 
         {
             auto e = registry->create();
