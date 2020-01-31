dc4833d024d442dd53682f4dbd710f6b414ab92c
Mat <mathew.kostrzewa@ontariotechu.net>
2020-01-31 11:34:28 -0500

    Fully functional garbage HP bars, also added delay to garbage cleaning
    Also made it so that full depleted garbage piles do not show an invalid reticle when the player is looking at it

--- Game/src/CustomComponents.h
+++ Game/src/CustomComponents.h
@@ -96,7 +96,7 @@ struct Player
 	MoveableUsingLerp adjustingPositionStateData;
 	MoveableUsingLerp pushingStateData;
 
-	float CLEANING_TIME_DURATION = 1.2f;
+	float CLEANING_TIME_DURATION = 1.2f; //IF YOU CHANGE THIS, MAKE SURE TO ALSO CHANGE THE DEPENDANT VALUES IN GARBAGE PILE AND GARBAGE HP BAR COMPONENETS (check the comments in those components to figure out which ones)
 	float cleaningTimeCountdown = CLEANING_TIME_DURATION;
 
 	float yRotationClamp = 0.0f;
@@ -192,8 +192,11 @@ struct GarbagePile
 	float GARBAGE_TICKS_PER_LEVEL = 4.0f;
 	float garbageTicks = GARBAGE_TICKS_PER_LEVEL;
 
-	float DELAY_BEFORE_ADDING_GARBAGE_DURATION = 1.0f;
-	float delayBeforeAddingGarbageCountdown    = DELAY_BEFORE_ADDING_GARBAGE_DURATION;
+	float DELAY_BEFORE_ADDING_GARBAGE_DURATION = 1.0f; //this is used to add garbage to a pile that a cannon has shot at, or any other reason a delay could be wanted before adding garbage
+	float delayBeforeAddingGarbageCountdown    = -1.0f; //timer shouldnt start at the beginning of the game
+
+	float DELAY_BEFORE_REMOVING_GARBAGE_DURATION = 1.2f;  //this should be equal to the player's time it takes to clean
+	float delayBeforeRemovingGarbageCountdown    = -1.0f; //timer shouldnt start at the beginning of the game
 
 	int relativePositionOnShip = -10; //this will be -1 (left), 0 (middle), or 1 (right) to mirror the cannon track position. It is used to determine which pile the cannons are firing at
 };
@@ -221,13 +224,19 @@ struct EndScreen
 
 struct GarbagePileHealthBar
 {
+	entt::entity outlineEntity; //the outline of the HP bar
+
 	Team team;
 	PlayerNumber playerNum = PlayerNumber::One; //player to render to
 	int garbagePileNum = 0;
 
-	float interpolationSpeed = 1.2f;
-	float interpolationParam = 1.0f; //start with the bar full
+	float interpolationSpeed = 0.8f; //this should be relative to the player's time it takes to clean (1 / (time it takes to clean) = the value this variable should be)
+	float interpolationParam = 0.0f;
+	float startValue         = 1.0f; //health bar starts at full
 	float targetValue        = 1.0f;
+
+	bool shouldHideAfterInterpolating = false;
+	bool shouldBeHidden               = false;
 };
 
 struct CleaningQuicktimeEventIndicator
