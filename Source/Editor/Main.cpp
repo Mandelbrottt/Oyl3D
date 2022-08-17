3012a836f37c0f9273bf329157e2e451e35c7600
Myles Cragg <cragg.myles@gmail.com>
2022-08-17 00:36:06 -0400

    Added Main files in editor so that we generated a .lib

---
+++ Source/Editor/Main.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+#include "Main.h"
+
+namespace Refly::Editor
+{
+	void
+	Init()
+	{
+				
+	}
+
+	void
+	Shutdown()
+	{
+		
+	}
+}
---
+++ Source/Editor/Main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Editor/Common.h"
+
+namespace Refly::Editor
+{
+	REFLY_EDITOR_EXPORT
+	extern
+	void
+	Init();
+	
+	REFLY_EDITOR_EXPORT
+	extern
+	void
+	Shutdown();
+}
