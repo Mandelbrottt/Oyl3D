9cf7cf061cf01ec583d669680a45451ad78e9a05
Myles Cragg <cragg.myles@gmail.com>
2023-03-09 22:34:26 -0500

    Add better documentation to projectReferences override

--- Scripts/Premake/Overrides.lua
+++ Scripts/Premake/Overrides.lua
@@ -3,8 +3,13 @@ require('vstudio')
 local p = premake
 local m = premake.vstudio.vc2010
 
+-- Override the project references section of the visual studio project
+-- By default, visual studio will try to copy the build output of projects to their dependencies
+-- This causes a failure if a project doesn't output any artifacts, such as a header only library
+-- This change lets us reference assemblies in-place
 premake.override(premake.vstudio.vc2010.elements, "projectReferences", function(base, prj, ref)
-    if prj.clr ~= p.OFF or (m.isClrMixed(prj) and ref and ref.kind ~=p.STATICLIB) then
+    -- Use default behaviour for SharedLib CLR projects
+    if prj.clr ~= p.OFF or (m.isClrMixed(prj) and ref and ref.kind ~= p.STATICLIB) then
         return {
             m.referenceProject,
             m.referencePrivate,
@@ -14,12 +19,17 @@ premake.override(premake.vstudio.vc2010.elements, "projectReferences", function(
             m.referenceUseLibraryDependences,
         }
     else
+        -- See https://learn.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-items?view=vs-2022
+        -- for more documentation
         return {
             m.referenceProject,
             function(prj, ref)
+                -- Don't copy the assembly to the dependant project
                 m.element("Private", nil, "false")
             end,
             function(prj, ref)
+                -- Don't include the assembly as a reference to dependant projects, since
+                -- all assemblies are output to the same directory in the end
                 m.element("ReferenceOutputAssembly", nil, "false")
             end,
         }
