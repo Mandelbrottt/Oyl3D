52131c0515574891fc2b536f93aab651eb5d7875
Myles Cragg <cragg.myles@gmail.com>
2023-08-29 21:24:31 -0400

    Change default cpp dialect

--- Scripts/Premake/Common.lua
+++ Scripts/Premake/Common.lua
@@ -148,7 +148,7 @@ end
 function applyCommonCppSettings(projectConfig)
     filename("%{prj.name}_" .. _ACTION)
     language "C++"
-    cppdialect "C++20"
+    cppdialect "C++17"
     staticruntime "off"
     floatingpoint "fast"
     
@@ -191,7 +191,6 @@ function applyCommonCppSettings(projectConfig)
 
     filter "toolset:msc*"
         disablewarnings {
-            "5050",
             "4251", -- member needs dll-interface to be used by clients of class
             "4275", -- non dll-interface used as base for dll-interface
         }
