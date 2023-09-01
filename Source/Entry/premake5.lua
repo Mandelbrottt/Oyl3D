ebb7df1c512406f7e214a650ff00b912ef402c60
Myles Cragg <cragg.myles@gmail.com>
2023-09-01 14:41:43 -0400

    Initial mockup of module markup

---
+++ Source/Entry/premake5.lua
@@ -0,0 +1,12 @@
+Rearm.Entry = {
+    Language = "C++",
+    Kind = "WindowedApp",
+    Dependencies = {
+        "Core",
+        "Rendering",
+        "Editor",
+    },
+    Premake = function()
+
+    end
+}
