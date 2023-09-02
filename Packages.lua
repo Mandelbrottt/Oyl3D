a02fc275d3f6edddf1c99220b7c20a7d60a8bb66
Myles Cragg <cragg.myles@gmail.com>
2023-09-01 23:37:01 -0400

    Add error message if the user is missing the vulkan sdk

--- Packages.lua
+++ Packages.lua
@@ -99,6 +99,9 @@ Dependencies = {
 }
 
 local VULKAN_SDK = os.getenv("VULKAN_SDK")
+if not VULKAN_SDK then
+    premake.error("Missing Vulkan SDK! Please install the Vulkan SDK!")
+end
 
 Libraries = {
     Vulkan = {
