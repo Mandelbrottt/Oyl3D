9ee98b491cf748ce86b798f4232ab6acc5c116eb
Myles Cragg <cragg.myles@gmail.com>
2024-06-17 22:57:21 -0400

    Add basic time implementation

---
+++ Source/Core/Core/Time/Time.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "Time.h"
+
+#ifdef OYL_WINDOWS
+#	include "Time_Windows.h"
+#endif
+
+namespace Oyl::Time::Detail
+{
+	float g_deltaTime         = 0.0f;
+	float g_unscaledDeltaTime = 0.0f;
+	float g_fixedDeltaTime    = 0.0f;
+
+	float g_elapsedTime         = 0.0f;
+	float g_unscaledElapsedTime = 0.0f;
+	float g_timeDifference      = 0.0f;
+
+	float              g_timeScale     = 1.0f;
+	std::atomic<float> g_timeScaleHint = 1.0f;
+
+	void
+	Init()
+	{
+		return Platform::Init();
+	}
+
+	void
+	Update()
+	{
+		return Platform::Update();
+	}
+}
---
+++ Source/Core/Core/Time/Time.h
@@ -0,0 +1,75 @@
+#pragma once
+
+namespace Oyl::Time
+{
+	namespace Detail
+	{
+		extern float g_deltaTime;
+		extern float g_unscaledDeltaTime;
+		extern float g_fixedDeltaTime;
+		
+		extern float g_elapsedTime;
+		extern float g_unscaledElapsedTime;
+		extern float g_timeDifference;
+
+		extern float g_timeScale;
+		extern std::atomic<float> g_timeScaleHint;
+
+		void
+		OYL_CORE_API
+		Init();
+
+		void
+		OYL_CORE_API
+		Update();
+	}
+
+	inline
+	float
+	ElapsedTime()
+	{
+		return Detail::g_elapsedTime;
+	}
+	
+	inline
+	float
+	UnscaledElapsedTime()
+	{
+		return Detail::g_unscaledElapsedTime;
+	}
+	
+	inline
+	float
+	DeltaTime()
+	{
+		return Detail::g_deltaTime;
+	}
+	
+	inline
+	float
+	UnscaledDeltaTime()
+	{
+		return Detail::g_unscaledDeltaTime;
+	}
+	
+	inline
+	float
+	FixedDeltaTime()
+	{
+		return Detail::g_fixedDeltaTime;
+	}
+	
+	inline
+	float
+	SmoothDeltaTime()
+	{
+		return Detail::g_deltaTime;
+	}
+	
+	inline
+	void
+	SetTimeScale(float a_scale)
+	{
+		Detail::g_timeScaleHint = a_scale;
+	}
+}
