2763b734a9a0bdc202bb4353fc533ae75271d224
Myles Cragg <cragg.myles@gmail.com>
2024-06-20 19:28:54 -0400

    Add more files to pch

--- Source/Core/pch.h
+++ Source/Core/pch.h
@@ -3,7 +3,16 @@
 #include <algorithm>
 #include <atomic>
 #include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
 #include <string>
+#include <thread>
+#include <unordered_map>
 #include <vector>
 
 #include "Core/Common.h"
+#include "Core/Typedefs.h"
