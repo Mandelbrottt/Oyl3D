4fa4e9549b86a322e0b5e1588a713f0ba8ac6f82
Myles Cragg <cragg.myles@gmail.com>
2024-06-26 21:01:14 -0400

    Modify Module Interface

--- Source/Core/Core/Application/Module.h
+++ Source/Core/Core/Application/Module.h
@@ -2,22 +2,48 @@
 
 #include "Core/Common.h"
 #include "Core/Types/TypeId.h"
+#include "ModuleRegistry.h"
 
 namespace Oyl
 {
 	class OYL_CORE_API Module
 	{
 	public:
-		explicit
-		Module(std::string a_name);
+		Module();
 		
 		virtual
 		~Module() = default;
 
+#	pragma region Public Interface
 		virtual
 		TypeId
 		GetTypeId() = 0;
+
+		template<typename TModule, typename... TArgs, std::enable_if_t<std::is_base_of_v<TModule, Module>> = true>
+		Module*
+		Register(TArgs&&... a_args);
 		
+		template<typename TModule, std::enable_if_t<std::is_base_of_v<TModule, Module>> = true>
+		Module*
+		Get();
+		
+		template<typename TModule, std::enable_if_t<std::is_base_of_v<TModule, Module>> = true>
+		bool
+		Remove();
+		 
+		virtual
+		bool
+		IsEnabled() { return m_enabled; }
+
+		virtual
+		void
+		SetEnabled(bool a_value) { m_enabled = a_value; }
+
+		virtual
+		std::string_view
+		GetName() const = 0;
+#	pragma endregion
+#	pragma region Callback functions
 		virtual
 		void
 		OnInit() {}
@@ -29,19 +55,10 @@ namespace Oyl
 		virtual
 		void
 		OnShutdown() {}
-		 
-		virtual
-		bool
-		IsEnabled() { return m_enabled; }
+#	pragma endregion
 
-		virtual
-		void
-		SetEnabled(bool a_value) { m_enabled = a_value; }
-		
 	private:
 		bool m_enabled = true;
-		
-		std::string m_debugName;
 	};
 }
 
