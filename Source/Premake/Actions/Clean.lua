dd2b8f531047a98fb50fe30bcc04be34809bf8b0
Myles Cragg <github@mandelbrottt.com>
2026-03-01 16:52:00 -0500

    Add make and ninja project generators to clean

--- Source/Premake/Actions/Clean.lua
+++ Source/Premake/Actions/Clean.lua
@@ -4,51 +4,61 @@ premake.modules.Clean = {}
 local m = premake.modules.Clean
 
 newoption {
-    trigger     = "packages",
-    description = "Compatible with 'clean' action. Cleans the package cache.",
-    category    = "clean"
+	trigger     = "packages",
+	description = "Compatible with 'clean' action. Cleans the package cache.",
+	category    = "clean"
 }
 
+
 newaction {
-    trigger = "clean",
-    description = "Deletes the build directory and all project files",
+	trigger = "clean",
+	description = "Deletes the build directory and all project files",
 
-    onWorkspace = function(wks) -- Keep onWorkspace to display completion time at the end
-        local filesToRemove = os.matchfiles(("%s/%s*.sln"):format(wks.location, wks.name))
-        for i, v in ipairs(filesToRemove) do
-            printf("Removing %s...", path.getrelative(os.getcwd(), v))
-            os.remove(v)
-        end
-    end,
-    onProject = function(prj)
-        if _OPTIONS['packages'] and string.contains(prj.location:lower(), Config.PackagesDir:lower()) then
-            print(("Removing Package '%s'..."):format(prj.name))
-            os.rmdir(prj.location)
-        else
-            local filesToRemove = table.join(
-                os.matchfiles(("%s/%s*.vcxproj*"):format(prj.location, prj.name)),
-                os.matchfiles(("%s/%s*.csproj*"):format(prj.location, prj.name))
-            )
-            for i, v in ipairs(filesToRemove) do
-                printf("Removing %s...", path.getrelative(os.getcwd(), v))
-                os.remove(v)
-            end
-        end
-    end,
-    execute = function()
-        if os.isdir("Build") then
-            print("Removing Build Directory...")
-            os.rmdir("Build")
-        end
+	onWorkspace = function(wks) -- Keep onWorkspace to display completion time at the end
+		local filesToRemove = table.join(
+			os.matchfiles(("%s/%s*.sln"):format(wks.location, wks.name)),
+			os.matchfiles(("%s/Makefile"):format(wks.location)),
+			os.matchfiles(("%s/%s*.make"):format(wks.location, wks.name)),
+			os.matchfiles(("%s/%s*.ninja"):format(wks.location, wks.name))
+		)
+		for _, v in ipairs(filesToRemove) do
+			printf("Removing %s...", path.getrelative(os.getcwd(), v))
+			os.remove(v)
+		end
+	end,
+	onProject = function(prj)
+		if _OPTIONS['packages'] and string.contains(prj.location:lower(), Config.PackagesDir:lower()) then
+			print(("Removing Package '%s'..."):format(prj.name))
+			os.rmdir(prj.location)
+		else
+			local filesToRemove = table.join(
+				os.matchfiles(("%s/%s*.vcproj*"):format(prj.location, prj.name)),
+				os.matchfiles(("%s/%s*.vcxproj*"):format(prj.location, prj.name)),
+				os.matchfiles(("%s/%s*.csproj*"):format(prj.location, prj.name)),
+				os.matchfiles(("%s/Makefile"):format(prj.location)),
+				os.matchfiles(("%s/%s*.make"):format(prj.location, prj.name)),
+				os.matchfiles(("%s/%s*.ninja"):format(prj.location, prj.name))
+			)
+			for _, v in ipairs(filesToRemove) do
+				printf("Removing %s...", path.getrelative(os.getcwd(), v))
+				os.remove(v)
+			end
+		end
+	end,
+	execute = function()
+		if os.isdir("Build") then
+			print("Removing Build Directory...")
+			os.rmdir("Build")
+		end
 
-        if _OPTIONS['packages'] then
-            local packagesToRemove = os.matchdirs(Config.PackagesDir .. "*")
-            for _, package in pairs(packagesToRemove) do
-                print(("Removing Orphan Package '%s'..."):format(path.getbasename(package)))
-                os.rmdir(package)
-            end
-        end
-    end,
+		if _OPTIONS['packages'] then
+			local packagesToRemove = os.matchdirs(Config.PackagesDir .. "*")
+			for _, package in pairs(packagesToRemove) do
+				print(("Removing Package '%s'..."):format(path.getbasename(package)))
+				os.rmdir(package)
+			end
+		end
+	end,
 
 }
 
