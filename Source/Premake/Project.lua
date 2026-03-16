ac268ef6ca14fb191dc8e5ca8a91c77276ed4281
Myles Cragg <github@mandelbrottt.com>
2026-03-16 15:09:39 -0400

    Better handling of Package.Include and filters

--- Source/Premake/Project.lua
+++ Source/Premake/Project.lua
@@ -2,19 +2,37 @@ local Project = {}
 
 ---@return any
 function Project.CurrentProject()
-	-- Recursively iterate up the scope tree until the project scope is found, else return nil
 	local scope = premake.api.scope.current
-	while true do
-		if not scope.parent then
-			return nil
-		end
-
-		if scope.class.name == "project" then
-			return scope
-		end
-		
-		scope = scope.parent
+	if scope.class.name == "project" then
+		return scope
+	else
+		return nil
 	end
+end
+
+---@param callback fun()
+function Project.NestedFilter(callback)
+	-- Cache the current scope, block, and block index
+	local scope = assert(Project.CurrentProject())
+	local currentBlock = scope.current
+	local blocks = scope.blocks
+	local lastBlockIndex = #blocks
+
+	-- Run the callback
+	callback()
+
+	-- Add existing filters to nested calls to filter
+	if #currentBlock._criteria.terms ~= 0 then
+		for i = lastBlockIndex + 1, #blocks, 1 do
+			local block = blocks[i]
+			local newTerms = table.join(block._criteria.terms, currentBlock._criteria.terms)
+			local crit = premake.criteria.new(newTerms)
+			block._criteria = crit
+		end
+	end
+
+	-- Restore outer filter
+	scope.current = currentBlock
 end
 
 ---@return string
