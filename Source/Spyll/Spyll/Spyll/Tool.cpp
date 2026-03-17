04789c4cf3374129f5bc36b19d94164b6e693d36
Myles Cragg <github@mandelbrottt.com>
2026-03-16 21:12:46 -0400

    Fix printing issues and memory scoping issues in clangtool

--- Source/Spyll/Spyll/Spyll/Tool.cpp
+++ Source/Spyll/Spyll/Spyll/Tool.cpp
@@ -7,8 +7,8 @@
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <llvm/Support/CommandLine.h>
 
-using namespace clang::tooling;
-using namespace llvm;
+namespace tooling = clang::tooling;
+namespace cl = llvm::cl;
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
@@ -17,48 +17,13 @@ static cl::OptionCategory MyToolCategory("my-tool options");
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static cl::extrahelp CommonHelp(tooling::CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nMore help text...\n");
 
 namespace Spyll
 {
-	Tool::Tool(std::vector<std::string> a_fileNames, std::string a_compileArgs)
-		: m_sources(std::move(a_fileNames))
-	{
-		std::replace(a_compileArgs.begin(), a_compileArgs.end(), ' ', '\n');
-		std::string error;
-		m_compilations = FixedCompilationDatabase::loadFromBuffer(
-			std::filesystem::current_path().string(),
-			a_compileArgs,
-			error
-		);
-
-		m_clangTool = std::make_unique<ClangTool>(
-			*m_compilations,
-			a_fileNames
-		);
-	}
-
-	Tool::Tool(std::string a_fileName, std::string a_compileArgs)
-		: Tool(std::vector { std::move(a_fileName) }, std::move(a_compileArgs)) {}
-
-	Tool::Tool(size_t argc, const char** argv)
-	{
-		int int_argc = (int) argc;
-		auto ExpectedOptions = CommonOptionsParser::create(int_argc, argv, MyToolCategory);
-		if (!ExpectedOptions)
-		{
-			llvm::errs() << ExpectedOptions.takeError();
-			return;
-		}
-		m_clangTool = std::make_unique<ClangTool>(
-			ExpectedOptions->getCompilations(),
-			ExpectedOptions->getSourcePathList()
-		);
-	}
-
 	Tool::~Tool() {}
 
 	int
@@ -66,8 +31,71 @@ namespace Spyll
 	{
 		if (m_clangTool)
 		{
-			m_errorCode = m_clangTool->run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+			m_errorCode = m_clangTool->run(tooling::newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 		}
 		return m_errorCode;
 	}
+
+	void
+	Tool::SetPrintErrorMessage(bool a_shouldPrintMessage)
+	{
+		if (m_clangTool)
+		{
+			m_clangTool->setPrintErrorMessage(a_shouldPrintMessage);
+			if (a_shouldPrintMessage)
+				m_clangTool->setDiagnosticConsumer(nullptr);
+			else
+				m_clangTool->setDiagnosticConsumer(m_diagnosticConsumer.get());
+		}
+	}
+
+	DirectTool::DirectTool(std::vector<std::string> a_fileNames, std::string a_compileArgs)
+		: m_sources(std::move(a_fileNames))
+	{
+		std::replace(a_compileArgs.begin(), a_compileArgs.end(), ' ', '\n');
+		std::string error;
+		m_compilations = tooling::FixedCompilationDatabase::loadFromBuffer(
+			std::filesystem::current_path().string(),
+			a_compileArgs,
+			error
+		);
+
+		m_clangTool = std::make_unique<tooling::ClangTool>(
+			*m_compilations,
+			m_sources
+		);
+
+		m_diagnosticConsumer = std::make_unique<clang::IgnoringDiagConsumer>();
+		m_clangTool->setDiagnosticConsumer(m_diagnosticConsumer.get());
+		m_clangTool->setPrintErrorMessage(false);
+	}
+
+	DirectTool::DirectTool(std::string a_fileName, std::string a_compileArgs)
+		: DirectTool(std::vector { std::move(a_fileName) }, std::move(a_compileArgs)) {}
+
+	DirectTool::~DirectTool() {}
+
+	CmdTool::CmdTool(int argc, const char** argv, bool a_printErrors)
+	{
+		using tooling::CommonOptionsParser;
+		using tooling::ClangTool;
+		auto ExpectedOptions = CommonOptionsParser::create(argc, argv, MyToolCategory);
+		if (!ExpectedOptions)
+		{
+			if (a_printErrors)
+				llvm::errs() << ExpectedOptions.takeError();
+			return;
+		}
+
+		CommonOptionsParser& OptionsParser = ExpectedOptions.get();
+		m_optionsParser = std::make_unique<CommonOptionsParser>(std::move(OptionsParser));
+		m_clangTool = std::make_unique<ClangTool>(
+			m_optionsParser->getCompilations(),
+			m_optionsParser->getSourcePathList()
+		);
+
+		SetPrintErrorMessage(a_printErrors);
+	}
+
+	CmdTool::~CmdTool() {}
 }
