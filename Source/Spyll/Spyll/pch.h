1563837323a0e1a3bd0690e68642ee1c1783e41f
Myles Cragg <github@mandelbrottt.com>
2026-03-18 20:17:44 -0400

    [Spyll] update pch

--- Source/Spyll/Spyll/pch.h
+++ Source/Spyll/Spyll/pch.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#pragma warning(push)
+#pragma warning(disable : 4702) // Unreachable code in MSVC
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Tooling/Tooling.h>
+#pragma warning(pop)
 
 #include <memory>
 #include <string>
