3d944085f5a95b063f226ad34f245b76b826b6c8
Myles Cragg <github@mandelbrottt.com>
2026-03-21 03:20:42 -0400

    [Spyll] Minor updates to descriptors

--- Source/Spyll/Spyll/ClangTool/Descriptors/TypeDescriptor.h
+++ Source/Spyll/Spyll/ClangTool/Descriptors/TypeDescriptor.h
@@ -1,15 +1,17 @@
 #pragma once
 
+#include "Descriptors/DescriptorId.h"
+
 namespace Spyll
 {
 	struct TypeDescriptor
 	{
-		DescriptorId id;
+		DescriptorId id = DescriptorId::Invalid;
 
 		std::string name;
-		uint32_t size;
-		bool isComposite;
-		bool isStruct;
+		uint32_t size = 0;
+		bool isComposite = false;
+		bool isStruct = false;
 
 		std::vector<DescriptorId> fields;
 		std::vector<DescriptorId> functions;
