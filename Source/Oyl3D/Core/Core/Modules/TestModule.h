e2c91bbb6eab1c014a9aecc77627e34687044ef8
Myles Cragg <github@mandelbrottt.com>
2026-04-06 21:37:09 -0400

    [Oyl3D] Comment out TestModule

--- Source/Oyl3D/Core/Core/Modules/TestModule.h
+++ Source/Oyl3D/Core/Core/Modules/TestModule.h
@@ -1,24 +1,24 @@
-#pragma once
-
-#include "../.Generated/Headers/TestModule.generated.h"
-
-#include "Core/Common.h"
-#include "Core/Application/Module.h"
-
-namespace Oyl
-{
-	class OYL_CORE_API TestModule : public Module
-	{
-		GeneratedBody();
-
-	public:
-		void
-		OnInit() override;
-
-		void
-		OnUpdate() override;
-
-		void
-		OnShutdown() override;
-	};
-}
+//#pragma once
+//
+////#include "../.Generated/Headers/TestModule.generated.h"
+//
+//#include "Core/Common.h"
+//#include "Core/Application/Module.h"
+//
+//namespace Oyl
+//{
+//	class OYL_CORE_API TestModule : public Module
+//	{
+//		GeneratedBody();
+//
+//	public:
+//		void
+//		OnInit() override;
+//
+//		void
+//		OnUpdate() override;
+//
+//		void
+//		OnShutdown() override;
+//	};
+//}
