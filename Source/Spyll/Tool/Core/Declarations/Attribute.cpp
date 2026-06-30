aaadee08173e6a9a20da5e90eabc378c9887af52
Myles Cragg <github@mandelbrottt.com>
2026-06-30 02:51:45 -0400

    [Spyll] Add parsing of constexpr attribute args, including typeid

--- Source/Spyll/Tool/Core/Declarations/Attribute.cpp
+++ Source/Spyll/Tool/Core/Declarations/Attribute.cpp
@@ -6,21 +6,57 @@ namespace Spyll
 {
 	AttributeParser::AttributeParser(const clang::Decl* a_decl)
 	{
-		for (auto attr : a_decl->attrs())
+		const auto& ctx = a_decl->getASTContext();
+
+		for (auto annotation : a_decl->specific_attrs<clang::AnnotateAttr>())
 		{
-			auto annotation = clang::dyn_cast<clang::AnnotateAttr>(attr);
-			if (!annotation)
+			auto category = annotation->getAnnotation();
+
+			if (category.compare("__ATTR__") != 0)
 			{
 				continue;
 			}
 
-			auto lexer = clang::Lexer(
-				annotation->getLocation(),
-				a_decl->getASTContext().getLangOpts(),
-				annotation->getAnnotation().data(),
-				annotation->getAnnotation().data(),
-				annotation->getAnnotation().data() + annotation->getAnnotationLength()
-			);
+			for (auto arg : annotation->args())
+			{
+				if (auto constExpr = clang::dyn_cast<clang::ConstantExpr>(arg))
+				{
+					Attribute attribute;
+
+					clang::Expr::EvalResult result;
+					constExpr->EvaluateAsConstantExpr(result, ctx);
+
+					auto desugaredType = constExpr->getType().getDesugaredType(ctx);
+					attribute.type = desugaredType.getAsString(ctx.getPrintingPolicy());
+
+					int i = 0;
+					for (const auto* field : desugaredType->getAsRecordDecl()->fields())
+					{
+						auto fieldTypeDecl = field->getType()->getAsTagDecl();
+						if (fieldTypeDecl && fieldTypeDecl->getQualifiedNameAsString() == "Oyl::Reflection::TypeId")
+						{
+							// Assume users will call a function with signature template<typename T> TypeId(*)()
+							auto cxxFunctional = clang::dyn_cast<clang::CXXFunctionalCastExpr>(constExpr->getSubExpr());
+							auto cxxConstruct = clang::dyn_cast<clang::CXXConstructExpr>(cxxFunctional->getSubExpr());
+							auto call = clang::dyn_cast<clang::CallExpr>(cxxConstruct->getArg(0));
+							auto callee = clang::dyn_cast<clang::FunctionDecl>(call->getCalleeDecl());
+							auto tempArg = callee->getTemplateSpecializationArgs()->get(0);
+
+							auto recordDecl = tempArg.getAsType()->getAsRecordDecl();
+							attribute.arguments.emplace_back("__TypeId " + recordDecl->getQualifiedNameAsString());
+						} else
+						{
+							auto apValue = result.Val.getStructField(i);
+							auto str = apValue.getAsString(ctx, field->getType());
+							attribute.arguments.emplace_back(std::move(str));
+						}
+
+						i++;
+					}
+
+					m_attributes.emplace_back(std::move(attribute));
+				}
+			}
 		}
 	}
 }
