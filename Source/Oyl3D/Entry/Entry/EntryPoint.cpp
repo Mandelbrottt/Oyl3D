4fd08fbd1a04227a4be08ff185916dbe993bba4a
Myles Cragg <github@mandelbrottt.com>
2026-07-03 15:08:32 -0400

    [Oyl3D] Remove static Module Registration functions, and test modules from entry point

--- Source/Oyl3D/Entry/Entry/EntryPoint.cpp
+++ Source/Oyl3D/Entry/Entry/EntryPoint.cpp
@@ -1,12 +1,10 @@
-#include <Windows.h>
 #include <cstdlib>
 #include <filesystem>
+#include <Windows.h>
 
 #include <Core/Application/CommandLine.h>
 #include <Core/Application/Main.h>
-#include <Core/Application/Module.h>
 #include <Core/Logging/Logging.h>
-#include <Core/Events/Event.h>
 #include <Core/Profiling/Profiler.h>
 
 //#include "Include.generated.h"
@@ -18,80 +16,6 @@ static void
 ShutdownConsole();
 
 static bool g_running = true;
-
-struct TestEvent1 : Oyl::Event
-{
-	OYL_DECLARE_EVENT(TestEvent1);
-
-	int a;
-};
-
-struct TestEvent2 : Oyl::Event
-{
-	OYL_DECLARE_EVENT(TestEvent2);
-
-	int b;
-};
-
-class TestModule1 : public Oyl::Module
-{
-	OYL_DECLARE_MODULE(TestModule1);
-
-public:
-	void
-	OnInit() override
-	{
-		OYL_LOG("TM1 {} Init!", GetName());
-
-		RegisterEventListener(&TestModule1::OnTestEvent1);
-	}
-
-	void
-	OnUpdate() override
-	{
-		OYL_LOG("TM1 {} Update!", GetName());
-
-		TestEvent2 e2;
-		e2.b = 6;
-		PostEvent(e2);
-	}
-
-	void
-	OnTestEvent1(TestEvent1& a_event)
-	{
-		OYL_LOG("TM1 a = {}", a_event.a);
-	}
-};
-
-class TestModule2 : public Oyl::Module
-{
-	OYL_DECLARE_MODULE(TestModule2);
-
-public:
-	void
-	OnInit() override
-	{
-		OYL_LOG("TM2 {} Init!", GetName());
-
-		RegisterEventListener(&TestModule2::OnTestEvent2);
-	}
-
-	void
-	OnUpdate() override
-	{
-		OYL_LOG("TM2 {} Update!", GetName());
-
-		TestEvent1 e1;
-		e1.a = 6;
-		PostEvent(e1);
-	}
-
-	void
-	OnTestEvent2(TestEvent2& a_event)
-	{
-		OYL_LOG("TM2 b = {}", a_event.b);
-	}
-};
 
 // ReSharper disable CppInconsistentNaming
 int WINAPI WinMain(
@@ -145,9 +69,6 @@ int WINAPI WinMain(
 	const char* startupString = "Startup";
 	OYL_FRAME_MARK_START(startupString);
 	Oyl::Detail::Init(initParams);
-
-	TestModule1::Register();
-	TestModule2::Register();
 	OYL_FRAME_MARK_END(startupString);
 
 	while (g_running)
