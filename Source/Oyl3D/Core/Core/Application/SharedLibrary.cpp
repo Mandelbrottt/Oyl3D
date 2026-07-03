7b4eacaeb3d69ddccc079eb4f299fb3c97392cf2
Myles Cragg <github@mandelbrottt.com>
2026-07-03 12:21:17 -0400

    [Oyl3D] Move SharedLibrary implementation to ifdefs in main cpp file
    Add NullPointer typetrait and concepts

--- Source/Oyl3D/Core/Core/Application/SharedLibrary.cpp
+++ Source/Oyl3D/Core/Core/Application/SharedLibrary.cpp
@@ -1,10 +1,23 @@
 #include "SharedLibrary.h"
 
+#include <cassert>
+
 #include "Core/Common.h"
 
 namespace Oyl
 {
-	inline
+	SharedLibrary::SharedLibrary(const std::string& a_libraryFileName) noexcept
+		: SharedLibrary()
+	{
+		LoadFromFileName(a_libraryFileName);
+	}
+
+	SharedLibrary::SharedLibrary(void* a_functionAddress) noexcept
+		: SharedLibrary()
+	{
+		LoadFromFunctionAddress(a_functionAddress);
+	}
+
 	void*
 	SharedLibrary::GetFunction(const std::string& a_functionName) const noexcept
 	{
@@ -55,7 +68,173 @@ namespace Oyl
 }
 
 #if defined(OYL_WINDOWS)
-#include "SharedLibrary_windows.inl"
-#elif defined(OYL_LINUX)
-#include "SharedLibrary_linux.inl"
+#include <Windows.h>
+
+namespace Oyl
+{
+	struct SharedLibrary::Impl
+	{
+		HINSTANCE instance = NULL;
+
+		bool refCounted = false;
+	};
+
+	SharedLibrary::SharedLibrary() noexcept
+		: m_impl(new Impl) {}
+
+	SharedLibrary::~SharedLibrary() noexcept
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		std::memset(&m_impl, 0, sizeof(Impl));
+		delete m_impl;
+		m_impl = nullptr;
+	}
+
+	bool
+	SharedLibrary::LoadFromFileName(const std::string& a_libraryFileName) noexcept
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		if (a_libraryFileName.empty())
+		{
+			m_impl->instance = GetModuleHandleA(NULL);
+			m_impl->refCounted = false;
+		} else
+		{
+			m_impl->instance = LoadLibraryA(a_libraryFileName.data());
+			m_impl->refCounted = true;
+		}
+		return m_impl->instance != NULL;
+	}
+
+	bool
+	SharedLibrary::Unload() noexcept
+	{
+		UnloadCommon();
+
+		if (!m_impl->instance)
+		{
+			return false;
+		}
+
+		BOOL result = TRUE;
+		if (m_impl->refCounted)
+		{
+			result = FreeLibrary(m_impl->instance);
+		}
+		*m_impl = Impl {};
+		return result;
+	}
+
+	bool
+	SharedLibrary::IsLoaded() const noexcept
+	{
+		return m_impl->instance != NULL;
+	}
+
+	void*
+	SharedLibrary::GetFunctionRaw(const std::string& a_functionName) const noexcept
+	{
+		FARPROC procAddress = GetProcAddress(m_impl->instance, a_functionName.c_str());
+		return reinterpret_cast<void*>(procAddress);
+	}
+
+	bool
+	SharedLibrary::LoadFromFunctionAddress(void* a_functionPtr) noexcept
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		HMODULE hModule = NULL;
+		GetModuleHandleExA(
+			GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+			(LPCSTR) a_functionPtr,
+			&hModule
+		);
+
+		if (hModule)
+		{
+			m_impl->refCounted = true;
+			m_impl->instance = hModule;
+		}
+		return m_impl->instance != NULL;
+	}
+}
+#endif // defined(OYL_WINDOWS)
+#if defined(OYL_LINUX)
+namespace Oyl
+{
+	struct SharedLibrary::Impl
+	{
+
+	};
+
+	SharedLibrary::SharedLibrary() noexcept
+		: m_impl(new Impl) {}
+
+	SharedLibrary::~SharedLibrary() noexcept
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		std::memset(&m_impl, 0, sizeof(Impl));
+		delete m_impl;
+		m_impl = nullptr;
+	}
+
+	bool
+	SharedLibrary::LoadFromFileName(const std::string& a_libraryFileName) noexcept
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		return NOT_IMPLEMENTED();
+	}
+
+	bool
+	SharedLibrary::Unload() noexcept
+	{
+		UnloadCommon();
+
+		NOT_IMPLEMENTED()
+
+		*m_impl = Impl {};
+	}
+
+	bool
+	SharedLibrary::IsLoaded() const noexcept
+	{;
+	return NOT_IMPLEMENTED();
+	}
+
+	void*
+	SharedLibrary::GetFunctionRaw(const std::string& a_functionName) const noexcept
+	{
+		return NOT_IMPLEMENTED();
+	}
+
+	bool
+	SharedLibrary::LoadFromFunctionAddress(void* a_functionPtr)
+	{
+		if (IsLoaded())
+		{
+			Unload();
+		}
+
+		return NOT_IMPLEMENTED();
+	}
+}
 #endif
