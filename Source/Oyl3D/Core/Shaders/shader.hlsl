090140e29f62c1c5141195df45d7e760b1c68979
Myles Cragg <github@mandelbrottt.com>
2026-07-05 06:05:22 -0400

    [Oyl3D] add hlsl shader
    TODO: Setup in premake

---
+++ Source/Oyl3D/Core/Shaders/shader.hlsl
@@ -0,0 +1,35 @@
+static float2 g_positions[3] = {
+	float2(0.0, -0.5),
+	float2(0.5, 0.5),
+	float2(-0.5, 0.5)
+};
+
+static float3 g_colors[3] = {
+	float3(1.0, 0.0, 0.0),
+	float3(0.0, 1.0, 0.0),
+	float3(0.0, 0.0, 1.0)
+};
+
+struct VertexOutput
+{
+	float4 sv_position : SV_Position;
+	float3 color : Color;
+};
+
+// /g/dev/VulkanSDK/1.4.341.1/bin/dxc -spirv -T vs_6_1 -fspv-target-env=vulkan1.3 -E VertMain ./shader.hlsl -Fo ./shader.vs.spv
+VertexOutput
+VertMain(uint a_vid : SV_VertexID)
+{
+	VertexOutput output;
+	output.sv_position = float4(g_positions[a_vid], 0.0, 1.0);
+	output.color = g_colors[a_vid];
+	return output;
+}
+
+// /g/dev/VulkanSDK/1.4.341.1/bin/dxc -spirv -T ps_6_1 -fspv-target-env=vulkan1.3 -E FragMain ./shader.hlsl -Fo ./shader.fs.spv
+float4
+FragMain(VertexOutput a_inVert) : SV_Target
+{
+	float3 color = a_inVert.color;
+	return float4(color, 1.0);
+}
