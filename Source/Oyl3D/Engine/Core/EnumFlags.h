b222650c42d77f1124c5c2c59e9af3bba2e7fd14
Myles Cragg <github@mandelbrottt.com>
2026-07-29 13:30:51 -0400

    [Oyl3D] Add EnumFlags

---
+++ Source/Oyl3D/Engine/Core/EnumFlags.h
@@ -0,0 +1,92 @@
+﻿#pragma once
+
+#include "TypeTraits.h"
+
+namespace Oyl
+{
+	template<Traits::Enum TBits>
+	class EnumFlags
+	{
+	public:
+		using TMask = Traits::TUnderlyingType<TBits>;
+
+		constexpr
+		EnumFlags() noexcept = default;
+
+		constexpr
+		EnumFlags(TBits a_bits) noexcept
+			: m_mask(static_cast<TMask>(a_bits)) {}
+
+		explicit
+		constexpr
+		EnumFlags(TMask a_mask) noexcept
+			: m_mask(a_mask) {}
+
+		EnumFlags(const EnumFlags&) noexcept = default;
+		EnumFlags(EnumFlags&&) noexcept = default;
+
+		constexpr
+		auto
+		operator <=>(const EnumFlags& a_rhs) const noexcept = default;
+
+		constexpr EnumFlags
+		operator |(EnumFlags a_rhs) const noexcept
+		{
+			return EnumFlags(m_mask | a_rhs);
+		}
+
+		constexpr EnumFlags
+		operator &(EnumFlags a_rhs) const
+		{
+			return EnumFlags(m_mask & a_rhs);
+		}
+
+		constexpr EnumFlags
+		operator ^(EnumFlags a_rhs) const
+		{
+			return EnumFlags(m_mask ^ a_rhs);
+		}
+
+		constexpr EnumFlags
+		operator ~() const
+		{
+			return EnumFlags(~0);
+		}
+
+		constexpr EnumFlags
+		operator =(EnumFlags a_rhs) const = default;
+
+		constexpr EnumFlags&
+		operator |=(EnumFlags a_rhs) const
+		{
+			return (*this = *this | a_rhs);
+		}
+
+		constexpr EnumFlags&
+		operator &=(EnumFlags a_rhs) const
+		{
+			return (*this = *this & a_rhs);
+		}
+
+		constexpr EnumFlags&
+		operator ^=(EnumFlags a_rhs) const
+		{
+			return (*this = *this ^ a_rhs);
+		}
+
+		explicit
+		constexpr operator bool() const
+		{
+			return !!m_mask;
+		}
+
+		explicit
+		constexpr operator TMask() const
+		{
+			return m_mask;
+		}
+
+	private:
+		TMask m_mask = {};
+	};
+}
